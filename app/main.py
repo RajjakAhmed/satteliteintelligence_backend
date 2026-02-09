86b4f34904d26b203cc651dba11f802cc1bd22bb
RajjakAhmed <46826387+RajjakAhmed@users.noreply.github.com>
2026-02-09 09:54:27 +0530

    Update CORS allowed origins for deployment

--- app/main.py
+++ app/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://satteliteintelligence.vercel.app"
+        "https://satteliteintelligence-omega.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
